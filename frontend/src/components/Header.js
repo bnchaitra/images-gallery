a8c041898b9182bc9a16413d0e7c8bc54bbe26ae
Chaitra <chaitra.bnc@gmail.com>
2026-09-19 12:28:17 +0530

    Welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Logo style={{ maxWidth: '12rem', maxHeigth: '2rem' }} />
+        <Logo alt={title} style={{ maxWidth: '12rem', maxHeigth: '2rem' }} />
       </Container>
     </Navbar>
   );
---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,21 @@
+import { React } from 'react';
+import { Button } from 'react-bootstrap';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Images Gallery</h1>
+      <p>
+        This is a simple application that retrieves photos using UnSplash API.
+        In order to start enter any search term in the input field.
+      </p>
+      <p>
+        <Button variant="primary" href="https://unsplash.com" target="_blank">
+          Learn more
+        </Button>
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
